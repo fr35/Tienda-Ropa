a51f8a14554ad84a661ba59f9220a3d6e13f501f
Facundo Rodriguez <facundorodriguezgranieri@gmail.com>
2022-06-27 18:34:56 -0300

    funcion mostrar productos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let carritoDeCompras = []
+
 const contenedorProductos = document.getElementById('contenedor-productos')
 const contenedorCarrito = document.getElementById('carrito-contenedor')
 
@@ -15,17 +17,26 @@ mostrarProductos()
 function mostrarProductos() {
     stockProductos.forEach(el=> {
         let div = document.createElement('div')
-        div.className = 'col-4'
+        div.className = 'col-4 mb-5'
         div.innerHTML = `   <div class="card" style="width: 18rem;">
-                                <img src="${el.img}" class="card-img-top" alt="...">
+                                <img src="${el.img}" class="card-img-top" alt="${el.nombre}">
                                 <div class="card-body">
                                     <h5 class="card-title">${el.nombre}</h5>
                                     <p class="card-subtitle mb-2 text-muted">${el.descrpcion}</p>
                                     <h5 class="card-title">$${el.precio}</h5>
-                                    <a href="#" class="btn btn-danger">Agregar al Carrito</a>
+                                    <a id="boton${el.id}" href="#" class="btn btn-danger">Agregar al Carrito</a>
                                 </div>
                             </div>`
         contenedorProductos.appendChild(div)
+        let btnAgregar = document.getElementById(`boton${el.id}`)
+        btnAgregar.addEventListener('click',()=>{
+            agregarAlCarrito(el.id)
+        })
     })
+}
+
+function agregarAlCarrito(id) {
+    let productoAgregar = stockProductos.find(item => item.id === id)
+    carritoDeCompras.push(productoAgregar)
 }
 
