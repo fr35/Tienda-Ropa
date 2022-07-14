6b9035f335b880ab45483845558e355c1a1cb46c
Facundo Rodriguez <facundorodriguezgranieri@gmail.com>
2022-07-14 03:21:41 -0300

    sidebar js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { mostrarProductos } from "./App.js";
+import { abrirSide } from "./side.js";
+
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
   mostrarProductos();
+  abrirSide()
 })
